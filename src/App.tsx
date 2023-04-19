c0424f0701ca650ee5cd471bb9331b3d21283b30
Jeremy Boetticher <me4jeremyboett@gmail.com>
2023-04-19 12:05:08 -0500

    Added token select (not functional)

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,22 @@
 import React, { useState, useEffect } from "react";
 import { CHAINS, transferFromEthNative } from "@certusone/wormhole-sdk";
-import { Container, Typography, Button, FormControl, Box, Card, CardContent } from "@mui/material";
+import { Container, Typography, Button, FormControl, Box, Card, CardContent, InputLabel, Select, MenuItem, SelectChangeEvent } from "@mui/material";
 import createMRLPayload, { MOONBEAM_ROUTED_LIQUIDITY_PRECOMPILE, Parachain, ETHEREUM_ACCOUNT_PARACHAINS } from "./MoonbeamRoutedLiquidityPayloads";
 import { FantomTestnet, useEtherBalance, useEthers } from '@usedapp/core'
 import { formatEther, parseEther } from '@ethersproject/units'
 import { providers } from 'ethers'
 
+enum Tokens {
+  FTM,
+  USDC
+}
+
 export default function () {
-  const { account, library } = useEthers();
+  const { account, library, chainId } = useEthers();
   const etherBalance = useEtherBalance(account)
   const [selectedNetwork, setSelectedNetwork] = useState(Parachain.MoonbaseBeta);
-  const [acc32, setAcc32] = useState("");
+  const [selectedToken, setSelectedToken] = useState<Tokens>(Tokens.FTM);
+  const [acc32, setAcc32] = useState(""); // TODO: input for Polkadot account
 
   const ParachainEntries = Object.entries(Parachain);
   const isEthereumStyledParachain = (x: Parachain) => ETHEREUM_ACCOUNT_PARACHAINS.includes(x);
@@ -29,12 +35,51 @@ export default function () {
     transferFromEthNative(
       "0x599CEa2204B4FaECd584Ab1F2b6aCA137a0afbE8",
       l.getSigner(),
-      parseEther("0.1"),
+      parseEther("1"),
       CHAINS.moonbeam,
       MOONBEAM_ROUTED_LIQUIDITY_PRECOMPILE,
-      0, // relayerFee, will be increased later
+      0, // relayerFee, doesn't matter because it's contract controlled
       undefined,
       payload
+    );
+  }
+
+  function SendTokensForm() {
+    return (
+      <Box>
+        <FormControl fullWidth variant="outlined" style={{ marginBottom: 12 }}>
+          <InputLabel htmlFor="network">Select a Network</InputLabel>
+          <Select
+            value={selectedNetwork}
+            onChange={(e: SelectChangeEvent<Parachain>) => setSelectedNetwork(e.target.value as Parachain)}
+            label="Select a Network"
+            inputProps={{ name: "network", id: "network" }}
+          >
+            {ParachainEntries.filter(x => isNaN(parseInt(x[0]))).map(x => (
+              <MenuItem key={x[0]} value={x[1]}>
+                {x[0]} ({ETHEREUM_ACCOUNT_PARACHAINS.includes(x[1] as Parachain) ? 'AccountKey20' : 'AccountId32'})
+              </MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+        <FormControl fullWidth variant="outlined" style={{ marginBottom: 12 }}>
+          <InputLabel htmlFor="token">Select a Token</InputLabel>
+          <Select
+            value={selectedToken}
+            onChange={(e: SelectChangeEvent<Tokens>) => setSelectedToken(e.target.value as Tokens)}
+            label="Select a Token"
+            inputProps={{ name: "token", id: "token" }}
+          >
+            {Object.entries(Tokens)
+              .filter(([key, value]) => isNaN(Number(key)))
+              .map(([key, value]) => (
+                <MenuItem key={key} value={value}>
+                  {key}
+                </MenuItem>
+              ))}
+          </Select>
+        </FormControl>
+      </Box>
     );
   }
 
@@ -46,24 +91,17 @@ export default function () {
             Wormhole Network Selector
           </Typography>
           <Typography variant="h6" gutterBottom textAlign='center'>
-            Transfer 0.1 FTM from <b>Fantom Testnet</b> ► Moonbase Alpha ► {ParachainEntries.find(([k, v]) => v === selectedNetwork)?.[0]}
+            Transfer 0.1 {Tokens[selectedToken]} from <b>Fantom Testnet</b> ► Moonbase Alpha ► {ParachainEntries.find(([k, v]) => v === selectedNetwork)?.[0]}
           </Typography>
-          <Box>
-            <FormControl fullWidth variant="outlined">
-              {/* ... (code remains unchanged) */}
-            </FormControl>
-          </Box>
+          <SendTokensForm />
           {etherBalance && (
-            <>
-              <div style={{ marginBottom: 8 }}>
-                <b>Balance: {formatEther(etherBalance)}</b>
-              </div>
+            <Box display="flex" justifyContent="center" alignItems="center">
               <Button variant="contained" onClick={handleXCMTransfer}>
                 Click to Transfer
               </Button>
-            </>
+            </Box>
           )}
-          <p>Ensure that you are connected to the Fantom Testnet.</p>
+          {chainId != FantomTestnet.chainId && <p>Ensure that you are connected to the Fantom Testnet.</p>}
           <ConnectButton />
         </CardContent>
       </Card>
@@ -77,7 +115,10 @@ const ConnectButton = () => {
 
   const style: React.CSSProperties = { position: 'absolute', top: 40, right: 40 };
 
-  if (chainId && chainId != FantomTestnet.chainId) switchNetwork(FantomTestnet.chainId);
+  useEffect(() => {
+    if (chainId && chainId != FantomTestnet.chainId) switchNetwork(FantomTestnet.chainId);
+    alert('awooga')
+  }, [chainId]);
 
   // Connecting to the wallet
   const handleWalletConnection = () => {
@@ -93,3 +134,4 @@ const ConnectButton = () => {
     </Box>
   );
 }
+

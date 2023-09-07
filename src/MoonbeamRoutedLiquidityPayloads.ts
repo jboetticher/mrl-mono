3bb738dc7dac34ad38839c4cfc7851548aa7e551
Jeremy Boetticher <me4jeremyboett@gmail.com>
2023-09-07 15:29:01 -0500

    Introduced enum

--- src/MoonbeamRoutedLiquidityPayloads.ts
+++ src/MoonbeamRoutedLiquidityPayloads.ts
@@ -1,20 +1,5 @@
 import { CHAINS, tryNativeToUint8Array } from '@certusone/wormhole-sdk';
-import { TypeRegistry, Enum, Struct } from '@polkadot/types';
 import { ApiPromise, WsProvider } from '@polkadot/api';
-
-const registry = new TypeRegistry();
-
-/**Moonbeam's MRL precompile expects a VersionedUserAction object as a payload.*/
-// class VersionedUserAction extends Enum {
-//   constructor(value?: any) {
-//     super(registry, { V1: XcmRoutingUserAction }, value);
-//   }
-// }
-// class XcmRoutingUserAction extends Struct {
-//   constructor(value?: any) {
-//     super(registry, { destination: "XcmVersionedMultilocation" }, value);
-//   }
-// }
 
 /**The parachain IDs of each parachain. Rarely subject to change */
 export enum Parachain {
@@ -40,6 +25,21 @@ export const SUBSTRATE_ACCOUNT_PARACHAINS = [
 export const MOONBEAM_ROUTED_LIQUIDITY_PRECOMPILE = tryNativeToUint8Array("0x0000000000000000000000000000000000000816", CHAINS.moonbeam);
 
 /**
+ * The runtime defined names for Substrate types relevant to MRL.  
+ * https://github.com/moonbeam-foundation/moonbeam/blob/acbd4e4f9e56b03563652d6e073ff0d0ff2ad98c/precompiles/gmp/src/types.rs#L25C1-L48C2
+*/
+enum MRLTypes {
+  /**Runtime defined MultiLocation. Allows for XCM versions 2 and 3. */
+  XcmVersionedMultiLocation = "XcmVersionedMultiLocation",
+  /**MRL payload (V1) that only defines the destination MultiLocation. */
+  XcmRoutingUserAction = "XcmRoutingUserAction",
+  /**MRL payload (V2) that defines the destination MultiLocation and a fee for the relayer. */
+  XcmRoutingUserActionWithFee = "XcmRoutingUserActionWithFee",
+  /**Wrapper object for the MRL payload. */
+  VersionedUserAction = "VersionedUserAction",
+};
+
+/**
  * Creates a payload that can instruct Moonbeam's Routed Liquidity precompile where to send data
  * @param {Parachain} parachainId The parachain ID of the parachain you wish to send tokens to
  * @param {string} account The account that you wish to send tokens to
@@ -62,24 +62,26 @@ export default async function createMRLPayload(parachainId: Parachain, account: 
     }
   };
 
-
+  // Creates an API for Moonbeam that defines MRL's special types
+  // 
   const wsProvider = new WsProvider('wss://wss.api.moonbase.moonbeam.network');
-  const api = await ApiPromise.create({ provider: wsProvider, types: {
-    XcmRoutingUserAction: { destination: "XcmVersionedMultilocation" },
-    VersionedUserAction: { V1: "XcmRoutingUserAction" }
-  }});
+  const api = await ApiPromise.create({
+    provider: wsProvider, types: {
+      [MRLTypes.XcmRoutingUserAction]: { destination: MRLTypes.XcmVersionedMultiLocation },
+      [MRLTypes.XcmRoutingUserActionWithFee]: { destination: MRLTypes.XcmVersionedMultiLocation, fee: 'u256' },
+      // [MRLTypes.VersionedUserAction]: { _enum: { V1: MRLTypes.XcmRoutingUserAction, V2: MRLTypes.XcmRoutingUserActionWithFee } }
+      [MRLTypes.VersionedUserAction]: { V1: MRLTypes.XcmRoutingUserAction }
+    }
+  });
 
   // Format multilocation object as a Polkadot.js type
-  const versionedMultilocation = api.createType('XcmVersionedMultiLocation', multilocation);
-  console.log('XcmVersionedMultiLocation', versionedMultilocation.toHex())
-  const routingUserAction = api.createType('XcmRoutingUserAction', { destination: versionedMultilocation });
-  console.log('XcmRoutingUserAction', routingUserAction.toHex())
+  const versionedMultilocation = api.createType(MRLTypes.XcmVersionedMultiLocation, multilocation);
+  const routingUserAction = api.createType(MRLTypes.XcmRoutingUserAction, { destination: versionedMultilocation });
 
   // Wrap and format the MultiLocation object into the precompile's input type
-  // const userAction = new XcmRoutingUserAction({ versionedMultilocation });
-  // const versionedUserAction = new VersionedUserAction({ V1: userAction });
-  // console.log("Versioned User Action JSON:", versionedUserAction.toJSON());
-  // console.log("Versioned User Action SCALE:", versionedUserAction.toHex());
+  const versionedUserAction = api.createType(MRLTypes.VersionedUserAction, { V2: routingUserAction });
+  console.log("Versioned User Action JSON:", versionedUserAction.toJSON());
+  console.log("Versioned User Action SCALE:", versionedUserAction.toHex());
 
   // SCALE encode resultant precompile formatted objects
   return routingUserAction.toU8a();
